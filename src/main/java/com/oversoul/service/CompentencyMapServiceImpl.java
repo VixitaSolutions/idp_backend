fd2c861a428f95a12a0bf5e1e8a531ab07a7df3f
avinashk <konduriavinashbala@gmail.com>
2023-04-10 20:26:20 +0530

    Implementation of Backend changes for excel Connectors

--- src/main/java/com/oversoul/service/CompentencyMapServiceImpl.java
+++ src/main/java/com/oversoul/service/CompentencyMapServiceImpl.java
@@ -1,7 +1,9 @@
 package com.oversoul.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.oversoul.entity.Competency;
 import com.oversoul.entity.CompetencyMap;
@@ -9,32 +11,59 @@ import com.oversoul.repository.CompetencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import lombok.extern.slf4j.Slf4j;
 
 import com.oversoul.Helper.ExcelHelper;
 import com.oversoul.repository.CompetencyMapRepository;
 
 @Service
+@Slf4j
 public class CompentencyMapServiceImpl{
     @Autowired
     CompetencyMapRepository repository;
     CompetencyRepository competencyRepo;
     private CompetencyMap competence;
+    private boolean noDuplicates = false;
 
 
     public void save(MultipartFile file, String tenantId) {
         try {
+            List<CompetencyMap> dummyList = new ArrayList<CompetencyMap>();
             List<CompetencyMap> tutorials = ExcelHelper.excelToTutorials(file.getInputStream());
+            List<CompetencyMap> TutorialsList = repository.findAll();
             //List<Competency> globalData = ExcelHelper.excelToGlobalCompetency(file.getInputStream());
             for(int i=0;i<tutorials.size();i++){
                 tutorials.get(i).setTenantId(tenantId);
             }
-            repository.saveAll(tutorials);
+            if(TutorialsList.size() > 0){
+                dummyList = this.createSharedListViaStream(tutorials, TutorialsList);
+                tutorials.removeAll(dummyList);
+                repository.saveAll(tutorials);
+            }
+            else {
+                repository.saveAll(tutorials);
+            }
+
            //competencyRepo.saveAll(globalData);
         } catch (IOException e) {
             throw new RuntimeException("fail to store excel data: " + e.getMessage());
         }
     }
 
+    public static List<CompetencyMap> createSharedListViaStream(List<CompetencyMap> listOne, List<CompetencyMap> listTwo)
+    {
+        // We create a stream of elements from the first list.
+        List<CompetencyMap> listOneList = listOne.stream()
+                // We select any elements such that in the stream of elements from the second list
+                .filter(two -> listTwo.stream()
+                        // there is an element that has the same name and school as this element,
+                        .anyMatch(one -> one.getcName().equals(two.getcName())
+                                && two.getTenantId().equals(one.getTenantId())))
+                // and collect all matching elements from the first list into a new list.
+                .collect(Collectors.toList());
+        // We return the collected list.
+        return listOneList;
+    }
     public List<CompetencyMap> getAllTutorials() {
         return repository.findAll();
     }
